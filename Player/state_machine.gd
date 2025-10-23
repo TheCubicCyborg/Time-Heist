f5028f6ff784d74ded7e3fe5cea45881d543077c
Daniel Salameh <danielsalameh04@gmail.com>
2025-10-23 12:38:08 -0700

    movement
    switching to state machine

---
+++ Player/state_machine.gd
@@ -0,0 +1,33 @@
+extends Node
+
+@export var starting_state: State
+
+var current_state: State
+# Called when the node enters the scene tree for the first time.
+func init(parent: Player) -> void:
+	for child in get_children():
+		child.parent = parent
+	
+	change_state(starting_state)
+
+func change_state(new_state : State) -> void:
+	if current_state:
+		current_state.exit()
+		
+	current_state = new_state
+	current_state.enter()
+	
+func handle_input(event: InputEvent) -> void:
+	var new_state = current_state.process_input(event)
+	if new_state:
+		change_state(new_state)
+
+func handle_physics(delta: float) -> void:
+	var new_state = current_state.process_physics(delta)
+	if new_state:
+		change_state(new_state)
+		
+func handle_frame(delta: float) -> void:
+	var new_state = current_state.process_frame(delta)
+	if new_state:
+		change_state(new_state)
