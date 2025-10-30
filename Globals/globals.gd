6aabfd9af08a702b0d77198d0b525279f93ac446
TheCubicCyborg <alexg555.ag@gmail.com>
2025-10-30 13:17:17 -0700

    Implemented Door

--- Globals/globals.gd
+++ Globals/globals.gd
@@ -1,6 +1,18 @@
 extends Node
 
+#Collision Layers:
+#1: Player
+#2: Interaction
+#3: Floor
+#4: Walls
+#5:
+#6:
+#7:
+#8:
+
 var time_manager: TimeManager
+
+var allow_interact: bool = true
 
 func _ready():
 	time_manager = preload("res://Modules/TimeTravel/TimeManager.gd").new()
