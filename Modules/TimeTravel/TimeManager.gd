59ecd9097843c3e9cc1236509a90865c172f9562
TheCubicCyborg <alexg555.ag@gmail.com>
2025-11-02 19:52:29 -0800

    Set up some basic UI and debug time control

--- Modules/TimeTravel/TimeManager.gd
+++ Modules/TimeTravel/TimeManager.gd
@@ -3,18 +3,22 @@ extends Node
 class_name TimeManager
 
 var time_stack: Array[TimeDelta] = []
-var cur_time: int = 0
+var cur_time: float = 0
 var paused: bool = true
-var reversing: bool = false
-
+var logging: bool = false
+var time_multiplier:float = 1
 
 
 func _ready():
-	print("Time Manager Ready!")
+	logging = true
+	pass
 
 func _physics_process(delta):
 	if !paused:
-		cur_time += delta
+		cur_time += delta * time_multiplier
+
+func toggle_time():
+	paused = not paused
 
 func start_time():
 	paused = false
@@ -24,6 +28,7 @@ func stop_time():
 
 func restart_time():
 	paused = true
+	time_stack.clear()
 	cur_time = 0
 
 func timelog(_object: Node,_var_name:String, _old_value, _new_value):
@@ -32,22 +37,29 @@ func timelog(_object: Node,_var_name:String, _old_value, _new_value):
 	newDelta.time_stamp = cur_time
 	newDelta.var_name = _var_name
 	newDelta.old_value = _old_value
-	newDelta.new_value = _new_value
 	time_stack.append(newDelta)
 
 func rewind(time_sec:float):
 	var goal_time = cur_time - time_sec
-	reversing = true
-	while(not time_stack.is_empty() and time_stack.back().time_stamp > goal_time):
-		undo_delta(time_stack.pop_back())
-	reversing = false
+	if goal_time < 0:
+		goal_time = 0
 
-func undo_delta(time_delta: TimeDelta):
-	pass
+	logging = false
+	while(not time_stack.is_empty() and time_stack.back().time_stamp > goal_time):
+		time_stack.pop_back().undo_delta()
+	logging = true
+	cur_time = goal_time
+
+func start_fast_forward(multiplier: float):
+	time_multiplier = multiplier
+
+func stop_fast_forward():
+	time_multiplier = 1
 
 class TimeDelta:
-	var object: Node
+	var object: Object
 	var time_stamp:int
 	var var_name:String
 	var old_value
-	var new_value
+	func undo_delta():
+		object.set(var_name,old_value)
