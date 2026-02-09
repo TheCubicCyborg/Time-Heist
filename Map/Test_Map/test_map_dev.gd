1c197e84097c2777b8590a2949d43bd6cff5fbed
Dev Patel <devp2@uci.edu>
2026-02-08 21:41:26 -0800

    Add pop up UI between dialogue options

--- Map/Test_Map/test_map_dev.gd
+++ Map/Test_Map/test_map_dev.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
 var resource := load("res://Dialogue/test.dialogue")
+var dialogue_balloon
+var special_ui
 
 func _ready() -> void:
-	DialogueManager.show_example_dialogue_balloon(resource, "start")
+	special_ui = $CanvasLayer/Panel
+	dialogue_balloon = DialogueManager.show_example_dialogue_balloon(resource, "start")
+
+func open_menu() -> void:
+	special_ui.show()
+	await special_ui.closed_signal
