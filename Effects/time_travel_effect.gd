2fb829431c4f5779ab4e5a99e0835336e6903bb3
Lucas Zhong <zhonglucas@gmail.com>
2026-02-09 20:31:19 -0800

    updated time travel effect

--- Effects/time_travel_effect.gd
+++ Effects/time_travel_effect.gd
@@ -1,20 +1,28 @@
 extends ColorRect
 
-@export var duration : float = 2.0
+@export var duration : float = 1.0
 var is_playing_effect = false
 
 func _ready() -> void:
 	globals.time_manager.time_traveled.connect(_play_anim)
+	globals.time_manager.stopped_time_travel.connect(func(): is_playing_effect = false)
 
 func _play_anim() -> void:
 	if is_playing_effect:
 		return
 	is_playing_effect = true
 	var tween : Tween = create_tween()
-	tween.tween_method(_set_shader_radius, 0.0, 1.0, duration)
+	tween.tween_method(_set_shader_radius, 0.0, 0.8, duration)
 	await tween.finished
-	_set_shader_radius(0.0)
-	is_playing_effect = false
+	
+	while is_playing_effect:
+		await _pulsate_effect()
+
+func _pulsate_effect() -> void:
+	var tween : Tween = create_tween()
+	tween.tween_method(_set_shader_radius, 0.8, 0.3, duration / 2)
+	tween.tween_method(_set_shader_radius, 0.3, 0.8, duration / 2)
+	await tween.finished
 	
 func _set_shader_radius(value: float) -> void:
 	var mat : ShaderMaterial = material
