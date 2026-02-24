04fc48f8c8c818b5bf2cdcb93e870c423ccc9786
TheCubicCyborg <alexg555.ag@gmail.com>
2026-02-24 02:45:07 -0800

    Added branch action and face action

---
+++ addons/NPC Pathing/PathClasses/BranchAction.gd
@@ -0,0 +1,6 @@
+class_name BranchAction extends InstantAction
+
+@export_node_path() var object: NodePath
+@export var property_name: String = ""
+@export var is_false: bool = false
+@export var dest_path_id: int
--- addons/NPC Pathing/PathClasses/InteractVertexAction.gd
+++ addons/NPC Pathing/PathClasses/InteractVertexAction.gd
@@ -1,9 +1,4 @@
 @tool
-class_name InteractVertexAction extends VertexAction
+class_name InteractVertexAction extends InstantAction
 
 @export_node_path("Interactable") var interactable: NodePath
-
-signal interact_signal(nodepath: NodePath)
-
-func interact():
-	interact_signal.emit(interactable)
