9cab788434ab252d6b7e7a40c182531b46a1561a
Dev Patel <devp2@uci.edu>
2026-02-23 16:53:18 -0800

    Fix interact and update homebase to not have repeated overlapping dialogues

--- Scenes/Homebase/friend.gd
+++ Scenes/Homebase/friend.gd
@@ -6,8 +6,16 @@ var dial_start_loc := "start"
 var dialogue_balloon
 
 func interact():
+	if dialogue_balloon and is_instance_valid(dialogue_balloon):
+		print("Already talking to friend")
+		return
+		
 	print("Talking to friend")
-	show_friend_dialogue()
-
-func show_friend_dialogue() -> void:
 	dialogue_balloon = DialogueManager.show_dialogue_balloon_scene(balloon_scene, resource, dial_start_loc)
+	dialogue_balloon.tree_exited.connect(_on_dialogue_finish)
+	
+func _on_dialogue_finish() -> void:
+	dialogue_balloon = null
+
+func _on_interactable_anon_interacted() -> void:
+	interact()
--- Scenes/Homebase/leave_area.gd
+++ Scenes/Homebase/leave_area.gd
@@ -6,6 +6,16 @@ var dial_start_loc := "start"
 var dialogue_balloon
 
 
+	
+func _on_dialogue_finish() -> void:
+	dialogue_balloon = null
+
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.name == "Player":
+		if dialogue_balloon and is_instance_valid(dialogue_balloon):
+			print("Already trying to leave")
+			return
+		
+		print("Trying to leave")
 		dialogue_balloon = DialogueManager.show_dialogue_balloon_scene(balloon_scene, resource, dial_start_loc)
+		dialogue_balloon.tree_exited.connect(_on_dialogue_finish)
