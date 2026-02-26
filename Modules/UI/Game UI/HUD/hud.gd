f00a0c98f589165ce295de27fe8f1ac2adbcdc6b
TheCubicCyborg <alexg555.ag@gmail.com>
2026-02-25 19:33:48 -0800

    added time to hud

--- Modules/UI/Game UI/HUD/hud.gd
+++ Modules/UI/Game UI/HUD/hud.gd
@@ -3,14 +3,18 @@ class_name HUD
 
 @onready var time_juice: TextureProgressBar = $TimeJuice
 @onready var new_notif_texture: TextureRect = $TimeJuice/NewNotif
+@onready var time_label: Label = $Time
 var notif_on_tab : Array[bool] = [false,false,false]
 
 func _ready() -> void:
 	globals.new_in_device.connect(new_notif)
 	pass
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	time_juice.value = globals.time_juice
+	if globals.time_manager:
+		var cur_time: int = int(globals.time_manager.cur_time)
+		time_label.text = "%02d:%02d" % [cur_time/60,int(cur_time)%60]
 	pass
 
 func new_notif(value : bool, tab : globals.Device_Tabs):
