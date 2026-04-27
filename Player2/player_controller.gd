fda4d50e45ed481ddd314f926e1a9f6212297e9c
TheCubicCyborg <alexg555.ag@gmail.com>
2026-04-27 00:50:38 -0700

    moving things into states

--- Player2/player_controller.gd
+++ Player2/player_controller.gd
@@ -29,24 +29,17 @@ class_name Player2 extends CharacterBody3D
 @export var turn_speed: float = 15.0
 
 var camera_locked: bool = true
-var crouching: bool = false
-var rolling: bool = false
-var roll_timer: float = 0
-var walking: bool = false
 
-var player_facing: Vector3 = Vector3.ZERO
-var destination_rotation: float
+enum MoveStates {RUN, WALK, CROUCH, ROLL}
+var state: MoveStates = MoveStates.RUN
+var state_move: Callable
+
 
 func _ready():
-	animation_tree["parameters/RollTimeScale/scale"] = 1.5/roll_duration
-	globals.player2 = self
-	destination_rotation = mesh.rotation.y
+	pass
 
 func _process(_delta):
-	if not Engine.is_editor_hint():
-		#if camera_locked:
-			#mesh.rotation.y = camera_pivot.rotation.y
-		pass
+	pass
 
 func pan_camera_horizontally(angle):
 	camera_pivot.rotate(Vector3.UP, angle)
@@ -57,62 +50,88 @@ func unlock_camera():
 func lock_camera():
 	camera_locked = true
 
-func toggle_crouch():
-	crouching = not crouching
-	if crouching:
-		#reduce hitbox
-		animation_tree["parameters/CrouchBlendSpace/blend_position"] = 1
-	else:
-		animation_tree["parameters/CrouchBlendSpace/blend_position"] = 0
-		pass #return hitbox to normal
+func toggle_crouch(): #input manager interface function
+	pass
 
-func set_walk(val: bool):
-	walking = val
+func set_walk(val: bool): #input manager interface function
+	pass
 
-func roll():
-	if rolling:
-		return
-	if crouching:
-		crouching = false
-	rolling = true
-	animation_tree["parameters/WalkRollTransition/transition_request"] = "Roll"
-	roll_timer = roll_duration
-	velocity = player_facing * roll_speed
+func roll(): #input manager interface function
+	pass
 
-func move(input_dir: Vector2, delta):
-	if input_dir.is_zero_approx() and not rolling:
-		velocity = Vector3.ZERO
-		animation_tree["parameters/WalkBlendSpace/blend_position"].y = -1
-		animation_tree["parameters/CrouchBlendSpace/blend_position"] = 0
-		if crouching:
-			animation_tree["parameters/WalkCrouchBlend/blend_amount"] = 1
-		else:
-			animation_tree["parameters/WalkCrouchBlend/blend_amount"] = 0
-	else:
-		if rolling:
-			roll_timer -= delta
-			if roll_timer <= 0:
-				roll_timer = 0
-				rolling = false
-				animation_tree["parameters/WalkRollTransition/transition_request"] = "Movement"
-		else:
-			var speed: float = max_run_speed
-			if walking:
-				speed = max_walk_speed
-				animation_tree["parameters/WalkBlendSpace/blend_position"].y = 0
-				animation_tree["parameters/WalkCrouchBlend/blend_amount"] = 0
-			elif crouching:
-				speed = max_crouch_speed
-				animation_tree["parameters/WalkCrouchBlend/blend_amount"] = 1
-				animation_tree["parameters/CrouchBlendSpace/blend_position"] = 1
-			else:
-				animation_tree["parameters/WalkBlendSpace/blend_position"].y = 1
-				animation_tree["parameters/WalkCrouchBlend/blend_amount"] = 0
-			velocity = (Vector3(input_dir.x, 0, input_dir.y).normalized() * speed).rotated(Vector3.UP,camera_pivot.rotation.y)
-			destination_rotation = Vector3.FORWARD.signed_angle_to(velocity, Vector3.UP)
-			player_facing = velocity
-			player_facing.y = 0
-			player_facing = player_facing.normalized()
-		if mesh.rotation.y != destination_rotation:
-			mesh.rotation.y = lerp_angle(mesh.rotation.y, destination_rotation,0.5)
+func move(input_dir: Vector2, delta): #input manager calls this every physics_process frame with the pertinent input information
+	#anything that should happen for all states should go here
+	state_move.call(input_dir,delta)
 	move_and_slide()
+
+#region run
+func run_enter():
+	pass
+
+func run_move(input_dir: Vector2, delta):
+	pass
+#endregion
+
+#region walk
+func walk_enter():
+	pass
+
+func walk_move(input_dir: Vector2, delta):
+	pass
+#endregion
+
+#region crouch
+func crouch_enter():
+	pass
+
+func crouch_move(input_dir: Vector2, delta):
+	pass
+#endregion
+
+#region roll
+func roll_enter():
+	pass
+
+func roll_move(input_dir: Vector2, delta):
+	pass
+#endregion
+
+
+#var player_facing: Vector3 = Vector3.ZERO
+#var destination_rotation: float
+
+	#if input_dir.is_zero_approx() and not rolling:
+		#velocity = Vector3.ZERO
+		#animation_tree["parameters/WalkBlendSpace/blend_position"].y = -1
+		#animation_tree["parameters/CrouchBlendSpace/blend_position"] = 0
+		#if crouching:
+			#animation_tree["parameters/WalkCrouchBlend/blend_amount"] = 1
+		#else:
+			#animation_tree["parameters/WalkCrouchBlend/blend_amount"] = 0
+	#else:
+		#if rolling:
+			#roll_timer -= delta
+			#if roll_timer <= 0:
+				#roll_timer = 0
+				#rolling = false
+				#animation_tree["parameters/WalkRollTransition/transition_request"] = "Movement"
+		#else:
+			#var speed: float = max_run_speed
+			#if walking:
+				#speed = max_walk_speed
+				#animation_tree["parameters/WalkBlendSpace/blend_position"].y = 0
+				#animation_tree["parameters/WalkCrouchBlend/blend_amount"] = 0
+			#elif crouching:
+				#speed = max_crouch_speed
+				#animation_tree["parameters/WalkCrouchBlend/blend_amount"] = 1
+				#animation_tree["parameters/CrouchBlendSpace/blend_position"] = 1
+			#else:
+				#animation_tree["parameters/WalkBlendSpace/blend_position"].y = 1
+				#animation_tree["parameters/WalkCrouchBlend/blend_amount"] = 0
+			#velocity = (Vector3(input_dir.x, 0, input_dir.y).normalized() * speed).rotated(Vector3.UP,camera_pivot.rotation.y)
+			#destination_rotation = Vector3.FORWARD.signed_angle_to(velocity, Vector3.UP)
+			#player_facing = velocity
+			#player_facing.y = 0
+			#player_facing = player_facing.normalized()
+		#if mesh.rotation.y != destination_rotation:
+			#mesh.rotation.y = lerp_angle(mesh.rotation.y, destination_rotation,0.5)
